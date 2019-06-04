a3489cf7c8fdda26412da070ed4c27ae5b168d28
Yanfei Peng <peng.yan@husky.neu.edu>
2019-06-04 17:50:50 -0400

    add script

--- app.js
+++ app.js
@@ -123,6 +123,6 @@ app.use(indexRoutes);
 app.use(commentRoutes);
 app.use(campgroundRoutes);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
     console.log("yelp camp started");
 });
